802e97242fdebb06cc2490b69e94c1df21abaa6f
About Programmer <chrismalone@pm.me>
2022-07-09 20:53:42 -0500

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { BanListCreatorComponent } from './ban-list-creator/ban-list-creator.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'create-ban-list', pathMatch: 'full' },
+  { path: '**', redirectTo: 'create-ban-list', pathMatch: 'full' },
   { path: 'create-ban-list', component: BanListCreatorComponent },
   { path: 'about', component: AboutComponent }
 ];
